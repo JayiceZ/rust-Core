01457d5cf8f5feeb39159cbd60577b0da57ad1df
Jayice <1185430411@qq.com>
2021-12-20 14:57:05 +0800

    shutdown on RISC-V

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,17 @@ use core::fmt::Write;
 
 mod lang_items;
 
+//sys call
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 
+//sbi call
+const SBI_SHUTDOWN: usize = 8;
 
 
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize=0;
+    let mut ret: isize = 0;
     unsafe {
         llvm_asm!("ecall"
             : "={x10}" (ret)
@@ -28,6 +31,21 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
     ret
 }
 
+fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+    let mut ret = 0;
+    unsafe {
+        asm!("ecall"
+            : "={x10}" (ret)
+            : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+        );
+    }
+    ret
+}
+
+
+
+
+
 pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
@@ -36,14 +54,20 @@ pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
     syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
 
+pub fn shut_down()->!{
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    panic!("should not reach here");
+}
+
+
 
 
 
 // compiler will see it as entrance
 #[no_mangle]
 extern "C" fn _start() {
-    println!("Hello, world!");
-    sys_exit(9);
+    //println!("Hello, world!");   it base on linux kernel
+    shut_down();
 }
 
 
