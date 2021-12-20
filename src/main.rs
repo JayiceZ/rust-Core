487e651a31737db7246188a57be8374e9dd51f75
Jayice <1185430411@qq.com>
2021-12-20 14:57:40 +0800

    use llvm_asm!

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
 fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let mut ret = 0;
     unsafe {
-        asm!("ecall"
+        llvm_asm!("ecall"
             : "={x10}" (ret)
             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
         );
